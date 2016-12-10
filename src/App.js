f95f0e0893ec7fb7611108aaf54bd7772f5dc8c9
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-12-10 11:53:03 -0500

    ajax posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import 'whatwg-fetch'
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { posts: [] }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/posts', { method: 'POST' })
+      .then(res => res.json())
+      .then(posts => this.setState({ posts }))
+      .catch(console.error)
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +24,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <ul>
+            {this.state.posts.map(p => <li key={p.id}>{p.title}</li>)}
+          </ul>
+        </div>
       </div>
     );
   }
