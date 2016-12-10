8fc84e7ab52df263d0e8176db407f235ba22c8da
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-12-10 12:01:03 -0500

    simple ajax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import 'whatwg-fetch'
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -14,16 +13,12 @@ class App extends Component {
     fetch('http://disorat.com/posts', { method: 'POST' })
       .then(res => res.json())
       .then(posts => this.setState({ posts }))
-      .catch(console.error)
   }
 
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <div className="App-header"></div>
         <div className="App-intro">
           <ul>
             {this.state.posts.map(p => <li key={p.id}>{p.title}</li>)}
@@ -34,4 +29,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
