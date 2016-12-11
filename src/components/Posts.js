d2bf5e31d76222c12c32166dc77c499808619d7d
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-12-10 22:50:06 -0500

    colors

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,42 +3,45 @@
  */
 
 import React from 'react'
-// import Post from './Post'
 
-export default ({ posts }) =>
-  <div className="row">
-    {posts.map(p =>
-      <div key={p.id} className="col-sm-6">
-        <div style={{
-          margin: '1em'
-        }} className="panel panel-primary">
-          <div className="panel-heading">
-            <a href="#" className="panel-title">
-              {p.title}
-            </a>
+export default class extends React.Component {
+
+  halfSize() {
+    return {
+      width: '50%',
+      whiteSpace: 'normal'
+    }
+  }
+
+  render() {
+
+    const { posts } = this.props
+
+    return (
+      <div className="row">
+        {posts.map(p =>
+          <div key={p.id} className="col-sm-6">
+            <div style={{
+              margin: '1em'
+            }} className="panel panel-primary">
+              <div className="panel-heading">
+                <a href="#" className="panel-title">
+                  {p.title}
+                </a>
+              </div>
+              <div className="panel-body">
+                <img src="..." alt={p.title}/>
+                <p style={{
+                  display: 'table',
+                  margin: '0 auto'
+                }}>{p.option1votes + p.option2votes} votes</p>
+                <button style={this.halfSize()} className="btn btn-info">{p.option1}</button>
+                <button style={this.halfSize()} className="btn btn-warning pull-right">{p.option2}</button>
+              </div>
+            </div>
           </div>
-          <div className="panel-body">
-            <img src="..." alt={p.title}/>
-            <p>{p.option1votes + p.option2votes} votes</p>
-            <button className="btn">{p.option1}</button>
-            <button className="btn pull-right">{p.option2}</button>
-          </div>
-        </div>
+        )}
       </div>
-    )}
-  </div>
-
-// {/*<div key={p.id} className="col-sm-6 col-md-4">*/}
-// {/*<div className="thumbnail">*/}
-// {/*<img src="..." alt={p.title}/>*/}
-// {/*<div className="caption">*/}
-// {/*<h3>{p.title}</h3>*/}
-// {/*<p>{p.option1votes + p.option2votes} votes</p>*/}
-// {/*<p>*/}
-// {/*<button style={{ maxWidth: '50%' }} className="btn btn-info" role="button">{p.option1}</button>*/}
-// {/*<button style={{ maxWidth: '50%' }} className="btn btn-warning pull-right"*/}
-// {/*role="button">{p.option2}</button>*/}
-// {/*</p>*/}
-// {/*</div>*/}
-// {/*</div>*/}
-// {/*</div>*/}
+    )
+  }
+}
